1ca676eeb7fe8bf9b6c25c6906a3b42d5027d692
Ritesh <riteshsinha008@gmail.com>
2023-07-08 09:14:32 +0530

    done

--- index.js
+++ index.js
@@ -1,24 +1,4 @@
-//Rest API demo in node.js
 
-// var express = require('express');
-// var app = express();
-// var fs = require('fs');
-
-// app.get('/getUsers', function(req,res){
-//     fs.readFile(__dirname + "/" + "users.json", 'utf8', function(err,data){
-//         console.log(data);
-//         res.end(data);
-//     });
-
-// })
-
-// var server = app.listen (8080, function(){
-//     var host = server.address().address
-//     var port = server.address().port
-//     console.log("REST API demo app ",host,port)
-// })
-
-//after
 
 const jsonServer = require("json-server");
 const server = jsonServer.create();
